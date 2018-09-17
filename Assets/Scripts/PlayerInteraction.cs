ee44b8ace48124647bb6718597f3a9edf1be0ec5
Stephen Hogan <stevyhogan@gmail.com>
2018-09-16 23:12:27 -0600

    Initial commit. Added CatMovement, WallJump, ButtonTrigger

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerInteraction : MonoBehaviour
+{
+
+    public string playerTag = "Player";
+
+    public UnityEvent playerInteractionEvent;
+
+    public bool continuousInteraction = false;
+
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            playerInteractionEvent.Invoke();
+        }
+    }
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if (continuousInteraction && other.CompareTag(playerTag))
+        {
+            playerInteractionEvent.Invoke();
+        }
+    }
+}
