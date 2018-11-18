ca10e9434ab0b32fb21dc7252f528bf560d6cba3
Stephen Hogan <stevyhogan@gmail.com>
2018-11-18 12:33:36 -0700

    Added the collectible assets and made them move/rotate. Started working on adding the glowing cat paw indicator.

---
+++ Assets/Scripts/GlowingCatPaw.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowingCatPaw : MonoBehaviour {
+
+    public float colliderOffset;
+
+    private BoxCollider2D collider2D;
+    
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Initialize(Transform parent)
+    {
+        collider2D = this.gameObject.GetComponent<BoxCollider2D>();
+        Vector2 parentCollider = parent.localScale;
+        collider2D.size = new Vector2(parentCollider.x + colliderOffset, parentCollider.y);
+        Debug.Log("parentColliderSize: " + parentCollider + " | childColliderSize: " + collider2D.size, this);
+    }
+}
---
+++ Assets/Scripts/GlowingCatPawManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowingCatPawManager : MonoBehaviour {
+
+    public GameObject pawPrefab;
+
+	// Use this for initialization
+	void Start () {
+        foreach( GameObject wall in GameObject.FindGameObjectsWithTag("Wall"))
+        {
+            Vector3 newPos = wall.transform.position;
+            newPos.z = -5;
+            GameObject catPaw = Instantiate(pawPrefab, newPos, wall.transform.rotation);
+            //catPaw.transform.SetParent(wall.transform);
+            catPaw.GetComponent<GlowingCatPaw>().Initialize(wall.transform);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
