bfa146c5218c4a09b178113348c6b5e68f9311f0
Stephen Hogan <stevyhogan@gmail.com>
2018-12-02 15:56:14 -0700

    Added some sounds into Level1. Going to add more

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour {
+
+    public AudioSource alternativeSource;
+    private AudioSource source;
+	// Use this for initialization
+	void Start () {
+        source = alternativeSource != null ? alternativeSource : this.GetComponent<AudioSource>();
+	}
+
+    public void PlaySound(AudioClip clip)
+    {
+        source.PlayOneShot(clip);
+    }
+}
